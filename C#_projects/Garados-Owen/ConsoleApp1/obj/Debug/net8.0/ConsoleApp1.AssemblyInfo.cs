d083792d55b6c293d7d46258eaad17d5f48f2f2e
Cryplonox-Owen <kenma.200599@gmail.com>
2026-09-20 19:02:36 +0800

    Organize image directories and apply responsive layout alignment updates

--- C#_projects/Garados-Owen/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ C#_projects/Garados-Owen/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90cce16d3cf7b45c8977bed9fa7aff3f9e409db9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfa1950d8e7ffa81225dd4a7f6efdc7700ff146a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
